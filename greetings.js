b7a68f2f6263610ccce059158a8847131f70bbe1
Duncan Reyneke <duncan.reyneke@lcef.org>
2025-10-20 13:31:00 -0500

    Add greetings.js practice file

---
+++ greetings.js
@@ -0,0 +1,17 @@
+function greet(name) {
+  const userName = "Alice";
+  let age = 30;
+  console.log(`Hello ${userName} Next year you'll be ${age + 1}`);
+}
+
+const newGreet = function (name) {
+  const userName = "Alice";
+  let age = 30;
+  console.log(`Hello ${userName} Next year you'll be ${age + 1}`);
+};
+
+const finalGreet = (name) => {
+  const userName = "Alice";
+  let age = 30;
+  console.log(`Hello ${userName} Next year you'll be ${age + 1}`);
+};
